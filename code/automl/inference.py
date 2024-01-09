a20e76a6352138181988dfe4614fc576d1d088ed
Kim Eunwoo <kimeunwoo0125@gmail.com>
2024-01-09 12:42:22 +0000

    [STYLE] train.py, inference.py을 컨벤션에 맞춤.
    #9

--- code/automl/inference.py
+++ code/automl/inference.py
@@ -7,7 +7,7 @@ data_path = "../../data/"
 
 # test Dataset 생성
 test_df = pd.read_csv(os.path.join(data_path, "test_data.csv"))
-test_df = test_df[test_df["answerCode"]==-1]
+test_df = test_df[test_df["answerCode"] == -1]
 test_data = TabularDataset(test_df)
 
 # load predictor
@@ -17,11 +17,11 @@ predictor = TabularPredictor.load("AutogluonModels/ag-20240109_091715")
 # inference
 
 answer = predictor.predict_proba(test_data)
-test_df = pd.concat((test_df, answer), axis = 1)
+test_df = pd.concat((test_df, answer), axis=1)
 # make a submission file
 write_path = os.path.join("outputs/", "submission.csv")
 os.makedirs(name="outputs", exist_ok=True)
 with open(write_path, "w", encoding="utf8") as w:
-        w.write("id,prediction\n")
-        for id, p in enumerate(test_df.iloc[:,-1]):
-            w.write("{},{}\n".format(id, p))
+    w.write("id,prediction\n")
+    for id, p in enumerate(test_df.iloc[:, -1]):
+        w.write("{},{}\n".format(id, p))
--- code/automl/train.py
+++ code/automl/train.py
@@ -8,12 +8,14 @@ data_path = "../../data/"
 train_df = pd.read_csv(os.path.join(data_path, "train_data.csv"))
 test_df = pd.read_csv(os.path.join(data_path, "test_data.csv"))
 df = pd.concat((train_df, test_df))
-train_df = df[df["answerCode"]>-1]
-test_df = df[df["answerCode"]==-1]
+train_df = df[df["answerCode"] > -1]
+test_df = df[df["answerCode"] == -1]
 
 # Read train data
 train_data = TabularDataset(train_df)
 test_data = TabularDataset(test_df)
 # Train
 label = "answerCode"
-predictor = TabularPredictor(label=label, eval_metric="roc_auc", problem_type="binary").fit(train_data, gpu_nums = 1, time_limit=600, presets=["medium_quality"])
+predictor = TabularPredictor(
+    label=label, eval_metric="roc_auc", problem_type="binary"
+).fit(train_data, gpu_nums=1, time_limit=600, presets=["medium_quality"])
