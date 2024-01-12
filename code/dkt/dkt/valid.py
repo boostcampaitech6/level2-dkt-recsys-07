3a6378819594b19a7d091cfe766d374825584a38
JunHyuck <wnsgur1615@naver.com>
2024-01-12 02:38:35 +0000

    [FIX] valid.py 수정
    #15

--- code/dkt/dkt/valid.py
+++ code/dkt/dkt/valid.py
@@ -4,4 +4,16 @@ def make_valid():
     test_df = pd.read_csv('../../data/test_data.csv')
     valid_df = test_df[test_df['answerCode'] != -1]
 
-    valid_df.to_csv('../../data/valid_data.csv')
+    valid_df.to_csv('../../data/valid_data.csv', index=False)
+
+def make_full_train():
+    train_df = pd.read_csv('../../data/train_data.csv')
+    valid_df = pd.read_csv('../../data/valid_data.csv')
+
+    full_train_df = pd.concat([train_df, valid_df], ignore_index=True)
+
+    full_train_df.to_csv('../../data/full_train_data.csv', index=False)
+
+if __name__ == 'main':
+    make_valid()
+    make_full_train()
