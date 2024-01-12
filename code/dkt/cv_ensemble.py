6c9d4d6b045d6d80bcb15834fc2b9e6f55436125
JunHyuck <wnsgur1615@naver.com>
2024-01-12 06:33:50 +0000

    [FEAT] cv_ensemble.py 추가
    #15

---
+++ code/dkt/cv_ensemble.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from dkt.args import parse_args
+
+def cv_ensemble(args):
+    df = pd.DataFrame()
+
+    for i in range(1,6):
+        cv = pd.read_csv(f'outputs/{args.model}_CV_{i}_submission.csv')
+        df[f'cv{i}'] = cv['prediction']
+
+    result = pd.DataFrame()
+    result['prediction'] = df.mean(axis='columns')
+    result.reset_index(inplace=True)
+    result.rename(columns={'index':'id'}, inplace=True)
+
+    result.to_csv(f'outputs/{args.model}_CV_submission.csv', index=False)
+
+if __name__ == "__main__":
+    args = parse_args()
+    cv_ensemble(args)
