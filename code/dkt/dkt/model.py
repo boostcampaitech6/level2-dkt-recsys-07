0e51b93c22a90ecfc20e3b71011c67f2bc9c4ccf
Kim Eunwoo <kimeunwoo0125@gmail.com>
2024-01-13 10:08:44 +0000

    [FEAT] LSTMATTN 모델도 선택한 범주형 피쳐를 입력받도록 수정
    #25

--- code/dkt/dkt/model.py
+++ code/dkt/dkt/model.py
@@ -57,48 +57,31 @@ class LSTM(ModelBase):
 
 
 class LSTMATTN(ModelBase):
-    def __init__(
-        self,
-        hidden_dim: int = 64,
-        n_layers: int = 2,
-        n_tests: int = 1538,
-        n_questions: int = 9455,
-        n_tags: int = 913,
-        n_heads: int = 2,
-        drop_out: float = 0.1,
-        **kwargs
-    ):
-        super().__init__(hidden_dim, n_layers, n_tests, n_questions, n_tags)
-        self.n_heads = n_heads
-        self.drop_out = drop_out
+    def __init__(self, args):
+        super().__init__(args)
+        self.n_heads = args.n_heads
+        self.drop_out = args.drop_out
         self.lstm = nn.LSTM(
-            self.hidden_dim, self.hidden_dim, self.n_layers, batch_first=True
+            args.hidden_dim, args.hidden_dim, args.n_layers, batch_first=True
         )
         self.config = BertConfig(
             3,  # not used
-            hidden_size=self.hidden_dim,
+            hidden_size=args.hidden_dim,
             num_hidden_layers=1,
             num_attention_heads=self.n_heads,
-            intermediate_size=self.hidden_dim,
-            hidden_dropout_prob=self.drop_out,
-            attention_probs_dropout_prob=self.drop_out,
+            intermediate_size=args.hidden_dim,
+            hidden_dropout_prob=args.drop_out,
+            attention_probs_dropout_prob=args.drop_out,
         )
         self.attn = BertEncoder(self.config)
 
-    def forward(self, test, question, tag, correct, mask, interaction):
-        X, batch_size = super().forward(
-            test=test,
-            question=question,
-            tag=tag,
-            correct=correct,
-            mask=mask,
-            interaction=interaction,
-        )
+    def forward(self, **input):
+        X, batch_size = super().forward(**input)
 
         out, _ = self.lstm(X)
         out = out.contiguous().view(batch_size, -1, self.hidden_dim)
 
-        extended_attention_mask = mask.unsqueeze(1).unsqueeze(2)
+        extended_attention_mask = input["mask"].unsqueeze(1).unsqueeze(2)
         extended_attention_mask = extended_attention_mask.to(dtype=torch.float32)
         extended_attention_mask = (1.0 - extended_attention_mask) * -10000.0
         head_mask = [None] * self.n_layers
