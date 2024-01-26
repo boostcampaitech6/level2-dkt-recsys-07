235cc9594d14bf4fa047ff970ecb8b82b20af6a9
JunHyuck <wnsgur1615@naver.com>
2024-01-26 10:20:31 +0000

    [FIX] CV 버그 수정

--- code/dkt/cv_ensemble.py
+++ code/dkt/cv_ensemble.py
@@ -6,7 +6,7 @@ def cv_ensemble(args):
     df = pd.DataFrame()
 
     for i in range(1, 6):
-        cv = pd.read_csv(f"outputs/{args.model.name}_CV_{i}_submission.csv")
+        cv = pd.read_csv(f"outputs/{args.model}_CV_{i}_submission.csv")
         df[f"cv{i}"] = cv["prediction"]
 
     result = pd.DataFrame()
@@ -14,7 +14,7 @@ def cv_ensemble(args):
     result.reset_index(inplace=True)
     result.rename(columns={"index": "id"}, inplace=True)
 
-    result.to_csv(f"outputs/{args.model.name}_CV_submission.csv", index=False)
+    result.to_csv(f"outputs/{args.model}_CV_submission.csv", index=False)
 
 
 if __name__ == "__main__":
