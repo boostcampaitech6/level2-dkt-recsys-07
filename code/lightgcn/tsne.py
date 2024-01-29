4eb59665b8a5fa4074e196c129c95664ecaf4e23
배준혁_T6072 <62535139+Juniork725@users.noreply.github.com>
2024-01-29 11:04:16 +0900

    Update tsne.py

--- code/lightgcn/tsne.py
+++ code/lightgcn/tsne.py
@@ -21,33 +21,6 @@ def main():
     model = TSNE(n_components=n)
     x_embeded = model.fit_transform(df)
     pd.DataFrame(x_embeded).to_csv('tsne_df.csv',index=False)
-    return
-
-    bigtag = np.array(list(map(lambda r: int(r[2]), train_df['assessmentItemID'].unique())))
-
-    fig = plt.figure(figsize=(8,8))
-    if n == 2:
-        ax = fig.add_subplot(111)
-        
-        ax.scatter(x_embeded[:,0],
-                    x_embeded[:,1],
-                    c = (bigtag-1)/9,
-                    s = 10,
-                    cmap = 'tab10',
-                    alpha=0.5)
-    elif n == 3:
-        ax = fig.add_subplot(111, projection='3d')
-
-        ax.scatter(x_embeded[:,0],
-                    x_embeded[:,1],
-                    x_embeded[:,2],
-                    c = (bigtag-1)/9,
-                    s = 10,
-                    cmap = 'tab10',
-                    alpha=0.5)
-    fig.colorbar(plt.cm.ScalarMappable(cmap='tab10'), ax = ax)
-
-    plt.savefig(f"{n}d_tsne.png")
 
 if __name__ == '__main__':
     main()
